394b92afce2b9f979a92b514e74771a9935dcc09
justnpT <Michal.Nowierski@wikia-inc.com>
2015-04-09 11:13:07 +0200

    adding setup.py

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setattr(
+    name = "strategos",
+    version = "0.1",
+    description = "experimental stock analysis tool",
+    author = "Michal Nowierski",
+    install_requires = ['pysocketio == 0.9.6a4']
+)
